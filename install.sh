94a6a7a9a20e3d2563beaea990c8781aa29e9927
death-grin <ee1366631ee@gmail.com>
2023-02-16 21:53:10 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+function enable_touchpad
+{
+    sudo ln -s /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+    sudo cp config/30-touchpad.conf /etc/X11/xorg.conf.d/.
+}
+
 echo "Coming soon..."
