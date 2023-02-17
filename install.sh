98f87e5d91f3b2fb552f7e51e558cbc9234d4792
death-grin <ee1366631ee@gmail.com>
2023-02-17 14:35:31 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+blue="\e[0;34m\033[1m"
+purple="\e[0;35m\033[1m"
+cyan="\e[0;36m\033[1m"
+reset="\033[0m\e[0m"
+
 directories=(bspwm kitty picom polybar sxhkd)
 
 function enable_touchpad
 {
+    echo -e "$blueROOT$reset is required to enable the $purpletouchpad$reset"
     sudo ln -s /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
     sudo cp config/30-touchpad.conf /etc/X11/xorg.conf.d/.
 }
@@ -12,7 +18,7 @@ function copy_config
 {
     for directory in ${directories[@]}; do
         cp -r config/$directory ~/.config/.
-        echo "$directory has been copied to ~/.config/$direcotry"
+        echo -e "[*] $blue$directory$reset has been copied to $purple~/.config/$directory$reset"
     done
 }
 
@@ -20,10 +26,19 @@ function rename_old_config
 {
     for directory in ${directories[@]}; do
         mv ~/.config/$directory ~/.config/$directory-old
-        echo "$directory has been renamed to $directory-old"
+        echo -e "[*] $blue$directory$reset has been renamed to $purple$directory-old$reset"
     done
 }
 
-echo "Coming soon..."
-copy_config
+clear
+echo "Dotfiles Installation"
+echo "Author: Death Grin"
+echo "Repository: https://github.com/death-grin/dotfiles"
+echo "---"
 rename_old_config
+echo "---"
+copy_config
+echo "---"
+enable_touchpad
+echo ""
+echo "Now $bluerestart$reset the session and log in with $purpleBSPWM$reset"
